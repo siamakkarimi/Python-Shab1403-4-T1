eb3f0a8d84022c72f50078c1244b895f654192b2
Siamak Karimi <Siyamak.Karimi@gmail.com>
2024-10-20 09:13:14 +0330

    CircleMas

---
+++ 01-InputPrintFormat.py
@@ -0,0 +1,11 @@
+fname = input("Enter name: ")
+lname = input("Enter family: ")
+currentYear = int(input("Enter emsal: "))
+birthdayYear = int(input("Enter tarikh tavalod: "))
+age = currentYear - birthdayYear
+
+print(f"your name is: {fname} and your family is: {lname} with age: {age}")
+
+
+
+
